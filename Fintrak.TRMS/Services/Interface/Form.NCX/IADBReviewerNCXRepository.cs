09aeb19c08c2a370b0c9d2877200076b181ad91d
kingkennyd17 <59066959+kingkennyd17@users.noreply.github.com>
2023-11-24 16:47:28 +0100

    Update IADBReviewerNCXRepository.cs

--- Fintrak.TRMS/Services/Interface/Form.NCX/IADBReviewerNCXRepository.cs
+++ Fintrak.TRMS/Services/Interface/Form.NCX/IADBReviewerNCXRepository.cs
@@ -1,10 +1,10 @@
-﻿using Fintrak.TRMS.Base;
+using Fintrak.TRMS.Base;
 using Fintrak.TRMS.Models;
 using System.Threading.Tasks;
 
 namespace Fintrak.TRMS.Services.Interface
 {
-    public interface IADBReviewerNCXRepository : IBaseRepository<ADBReviewerRequestNCX, ADBReviewerResponse>
+    public interface IADBReviewerNCXRepository : IBaseRepository<ADBReviewerRequest, ADBReviewerResponse>
     {
         Task<ADBReviewerResponse> ADBReviewerNCX(ADBReviewerRequestNCX adbReviewerNCXRequest, string applicationId, string token);
     }
